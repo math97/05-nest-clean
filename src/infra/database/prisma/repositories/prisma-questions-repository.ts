8328b5eef742417e8feca83dda47ae8218d33da8
Matheus Albuquerque <math.albuquerque@outlook.com>
2025-04-13 20:40:22 +0100

    chore: creating empty repositories to implement those on prisma

---
+++ src/infra/database/prisma/repositories/prisma-questions-repository.ts
@@ -0,0 +1,29 @@
+import { PaginationParams } from '@/core/repositories/pagination-params'
+import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository'
+import { Question } from '@/domain/forum/enterprise/entities/question'
+import { Injectable } from '@nestjs/common'
+
+@Injectable()
+export class PrismaQuestionsRepository implements QuestionsRepository {
+  create(question: Question): Promise<void> {}
+
+  save(question: Question): Promise<void> {
+    throw new Error('Method not implemented.')
+  }
+
+  findById(id: string): Promise<Question | null> {
+    throw new Error('Method not implemented.')
+  }
+
+  findBySlug(slug: string): Promise<Question | null> {
+    throw new Error('Method not implemented.')
+  }
+
+  findManyRecent(params: PaginationParams): Promise<Question[]> {
+    throw new Error('Method not implemented.')
+  }
+
+  delete(question: Question): Promise<void> {
+    throw new Error('Method not implemented.')
+  }
+}
