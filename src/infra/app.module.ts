7954c6da491db47ddb6f4e779359796ddcadb4a2
Matheus Albuquerque <math.albuquerque@outlook.com>
2025-06-01 13:46:08 +0100

    feat(env): creating an env service to be a helper instead of using configservice directly

--- src/infra/app.module.ts
+++ src/infra/app.module.ts
@@ -4,6 +4,7 @@ import { ConfigModule } from '@nestjs/config'
 import { envSchema } from './env'
 import { AuthModule } from './auth/auth.module'
 import { HttpModule } from './http/http.module'
+import { EnvModule } from './env/env.module'
 
 @Module({
   imports: [
@@ -13,8 +14,7 @@ import { HttpModule } from './http/http.module'
     }),
     AuthModule,
     HttpModule,
+    EnvModule,
   ],
-  controllers: [],
-  providers: [],
 })
 export class AppModule {}
