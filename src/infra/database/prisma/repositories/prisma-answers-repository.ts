f81b30041a31a962ec2dcca396863af80d128aef
Matheus Albuquerque <math.albuquerque@outlook.com>
2025-06-16 08:12:43 +0100

    test(notification): trigger domain events

--- src/infra/database/prisma/repositories/prisma-answers-repository.ts
+++ src/infra/database/prisma/repositories/prisma-answers-repository.ts
@@ -5,6 +5,7 @@ import { Injectable } from '@nestjs/common'
 import { PrismaAnswerMapper } from '../mappers/prisma-answer-mapper'
 import { PrismaService } from '../prisma.service'
 import { AnswerAttachmentsRepository } from '@/domain/forum/application/repositories/answer-attachments-repository'
+import { DomainEvents } from '@/core/events/domain-events'
 
 @Injectable()
 export class PrismaAnswersRepository implements AnswersRepository {
@@ -23,6 +24,8 @@ export class PrismaAnswersRepository implements AnswersRepository {
     await this.answerAttachmentsRepository.createMany(
       answer.attachments.getItems(),
     )
+
+    DomainEvents.dispatchEventsForAggregate(answer.id)
   }
 
   async save(answer: Answer): Promise<void> {
@@ -42,6 +45,8 @@ export class PrismaAnswersRepository implements AnswersRepository {
         answer.attachments.getRemovedItems(),
       ),
     ])
+
+    DomainEvents.dispatchEventsForAggregate(answer.id)
   }
 
   async findById(id: string): Promise<Answer | null> {
--- test/setup-e2e.ts
+++ test/setup-e2e.ts
@@ -2,6 +2,7 @@ import { config } from 'dotenv'
 import { randomUUID } from 'node:crypto'
 import { execSync } from 'node:child_process'
 import { PrismaClient } from '@prisma/client'
+import { DomainEvents } from '@/core/events/domain-events'
 
 config({ path: '.env', override: true })
 config({ path: '.env.test', override: true })
@@ -21,6 +22,7 @@ const schemaId = randomUUID()
 beforeAll(async () => {
   const databaseURL = generateUniqueDatabaseURL(schemaId)
   process.env.DATABASE_URL = databaseURL
+  DomainEvents.shouldRun = false
   execSync('npx prisma migrate deploy')
 })
 afterAll(async () => {
