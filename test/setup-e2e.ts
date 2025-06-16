749a0b5b95f696ae32d2461ab179b534842b0253
Matheus Albuquerque <math.albuquerque@outlook.com>
2025-06-16 08:54:24 +0100

    test(cache): testing cache persistent on prisma question repository

--- test/setup-e2e.ts
+++ test/setup-e2e.ts
@@ -3,26 +3,41 @@ import { randomUUID } from 'node:crypto'
 import { execSync } from 'node:child_process'
 import { PrismaClient } from '@prisma/client'
 import { DomainEvents } from '@/core/events/domain-events'
+import { envSchema } from '@/infra/env/env'
+import Redis from 'ioredis'
 
 config({ path: '.env', override: true })
 config({ path: '.env.test', override: true })
 
+const env = envSchema.parse(process.env)
+
 const prisma = new PrismaClient()
+const redis = new Redis({
+  host: env.REDIS_HOST,
+  port: env.REDIS_PORT,
+  db: env.REDIS_DB,
+})
 
 function generateUniqueDatabaseURL(schemaId: string) {
-  if (!process.env.DATABASE_URL) {
+  if (!env.DATABASE_URL) {
     throw new Error('Please provider a DATABASE_URL environment variable')
   }
-  const url = new URL(process.env.DATABASE_URL)
+  const url = new URL(env.DATABASE_URL)
+
   url.searchParams.set('schema', schemaId)
   return url.toString()
 }
 
 const schemaId = randomUUID()
+
 beforeAll(async () => {
   const databaseURL = generateUniqueDatabaseURL(schemaId)
   process.env.DATABASE_URL = databaseURL
+
   DomainEvents.shouldRun = false
+
+  await redis.flushdb()
+
   execSync('npx prisma migrate deploy')
 })
 afterAll(async () => {
